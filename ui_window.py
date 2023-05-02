1189aef10d097e5559ee6c0bffbc8d9a4e3203db
chicken10dys <chicken@10dys.ca>
2023-05-02 13:11:40 -0400

    Improved UI

--- ui_window.py
+++ ui_window.py
@@ -15,10 +15,9 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QFormLayout, QLayout, QMainWindow,
+from PySide6.QtWidgets import (QApplication, QHBoxLayout, QLayout, QMainWindow,
     QMenuBar, QProgressBar, QPushButton, QRadioButton,
-    QSizePolicy, QStatusBar, QTextEdit, QVBoxLayout,
-    QWidget)
+    QSizePolicy, QTextEdit, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -29,47 +28,57 @@ class Ui_MainWindow(object):
         MainWindow.setMaximumSize(QSize(400, 600))
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.formLayoutWidget = QWidget(self.centralwidget)
-        self.formLayoutWidget.setObjectName(u"formLayoutWidget")
-        self.formLayoutWidget.setGeometry(QRect(0, 470, 271, 41))
-        self.formLayout = QFormLayout(self.formLayoutWidget)
-        self.formLayout.setObjectName(u"formLayout")
-        self.formLayout.setSizeConstraint(QLayout.SetMaximumSize)
-        self.formLayout.setContentsMargins(0, 0, 0, 0)
-        self.browseBtn = QPushButton(self.formLayoutWidget)
-        self.browseBtn.setObjectName(u"browseBtn")
-
-        self.formLayout.setWidget(0, QFormLayout.LabelRole, self.browseBtn)
-
-        self.browseTxt = QTextEdit(self.formLayoutWidget)
-        self.browseTxt.setObjectName(u"browseTxt")
-
-        self.formLayout.setWidget(0, QFormLayout.FieldRole, self.browseTxt)
-
-        self.verticalLayoutWidget = QWidget(self.centralwidget)
-        self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
-        self.verticalLayoutWidget.setGeometry(QRect(70, 340, 271, 51))
-        self.verticalLayout = QVBoxLayout(self.verticalLayoutWidget)
-        self.verticalLayout.setObjectName(u"verticalLayout")
-        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
         self.eulaBtn = QRadioButton(self.centralwidget)
         self.eulaBtn.setObjectName(u"eulaBtn")
         self.eulaBtn.setGeometry(QRect(0, 0, 105, 22))
         self.progressBar = QProgressBar(self.centralwidget)
         self.progressBar.setObjectName(u"progressBar")
-        self.progressBar.setGeometry(QRect(0, 520, 401, 20))
+        self.progressBar.setGeometry(QRect(0, 550, 401, 20))
         self.progressBar.setValue(24)
-        self.createBtn = QPushButton(self.centralwidget)
+        self.horizontalLayoutWidget = QWidget(self.centralwidget)
+        self.horizontalLayoutWidget.setObjectName(u"horizontalLayoutWidget")
+        self.horizontalLayoutWidget.setGeometry(QRect(0, 489, 401, 51))
+        self.horizontalLayout = QHBoxLayout(self.horizontalLayoutWidget)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setSizeConstraint(QLayout.SetMaximumSize)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.browseBtn = QPushButton(self.horizontalLayoutWidget)
+        self.browseBtn.setObjectName(u"browseBtn")
+        sizePolicy = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(50)
+        sizePolicy.setHeightForWidth(self.browseBtn.sizePolicy().hasHeightForWidth())
+        self.browseBtn.setSizePolicy(sizePolicy)
+
+        self.horizontalLayout.addWidget(self.browseBtn)
+
+        self.browseTxt = QTextEdit(self.horizontalLayoutWidget)
+        self.browseTxt.setObjectName(u"browseTxt")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Maximum)
+        sizePolicy1.setHorizontalStretch(25)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.browseTxt.sizePolicy().hasHeightForWidth())
+        self.browseTxt.setSizePolicy(sizePolicy1)
+        self.browseTxt.setMinimumSize(QSize(0, 25))
+        self.browseTxt.setBaseSize(QSize(0, 25))
+
+        self.horizontalLayout.addWidget(self.browseTxt)
+
+        self.createBtn = QPushButton(self.horizontalLayoutWidget)
         self.createBtn.setObjectName(u"createBtn")
-        self.createBtn.setGeometry(QRect(280, 470, 121, 34))
+        sizePolicy2 = QSizePolicy(QSizePolicy.Maximum, QSizePolicy.Expanding)
+        sizePolicy2.setHorizontalStretch(0)
+        sizePolicy2.setVerticalStretch(50)
+        sizePolicy2.setHeightForWidth(self.createBtn.sizePolicy().hasHeightForWidth())
+        self.createBtn.setSizePolicy(sizePolicy2)
+
+        self.horizontalLayout.addWidget(self.createBtn)
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
         self.menubar.setGeometry(QRect(0, 0, 400, 30))
         MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QStatusBar(MainWindow)
-        self.statusbar.setObjectName(u"statusbar")
-        MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
 
@@ -78,8 +87,8 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Minecraft Server Maker", None))
-        self.browseBtn.setText(QCoreApplication.translate("MainWindow", u"Browse", None))
         self.eulaBtn.setText(QCoreApplication.translate("MainWindow", u"Accept eula", None))
+        self.browseBtn.setText(QCoreApplication.translate("MainWindow", u"Browse", None))
         self.createBtn.setText(QCoreApplication.translate("MainWindow", u"Create", None))
     # retranslateUi
 
